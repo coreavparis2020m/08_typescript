d0710f1730712bf7cb1ad1039f90b795b7b34f7e
coredummie2020 <coredummie2020@gmail.com>
2020-05-26 12:00:56 +0200

    initial

---
+++ js/01_tipado.js
@@ -0,0 +1,40 @@
+// Tipos de datos primitivos
+// String
+var mensaje; // Sintaxis de tipado let identificador: tipo;
+mensaje = 'Hola Mundo';
+console.log(mensaje);
+// Number
+var resultado;
+resultado = 6.7;
+// Boolean
+var mayorEdad;
+mayorEdad = true;
+var isPass = false; // El tipo se establece por inferencia
+// iPass = 'si'; // emite error porque el tipo es fuerte y aunque haya sido inferido no se puede modificar
+// Elementos de array
+var frutas; // El tipo será array de elementos string
+frutas = ['Pera', 'Manzana', 'Naranja'];
+var resultados; // Otra forma de tipar los elementos de un array
+resultados = [9.4, 10, 8, 6];
+// Any (para eliminar la inferencia)
+// let edad = '40'; 
+// edad = 37;
+var edad = '40'; // El tipo any permite cualquier tipo (volver a tipado dinámico)
+edad = 37;
+// Void para tipar que una función no devuelva nada
+function saludo() {
+    console.log('Hola Mundo!');
+    // return 'Hola Mundo!';
+}
+// Tipado de objetos
+var jugador1; // objeto de cualquier tipo
+jugador1 = {
+    nombre: 'Lionel',
+    apellidos: 'Messi'
+};
+var jugador2; // objeto de una determinada forma
+jugador2 = {
+    nombre: 'Cristiano',
+    apellidos: 'Ronaldo'
+    // edad: 40 no podemos poner propiedades que no existen en el tipo
+};
---
+++ js/01_tipado.ts
@@ -0,0 +1,64 @@
+// Tipos de datos primitivos
+
+// String
+
+let mensaje: string; // Sintaxis de tipado let identificador: tipo;
+
+mensaje = 'Hola Mundo';
+console.log(mensaje);
+
+// Number
+
+let resultado: number;
+resultado = 6.7;
+
+// Boolean
+
+let mayorEdad: boolean;
+mayorEdad = true;
+
+let isPass = false; // El tipo se establece por inferencia
+
+// iPass = 'si'; // emite error porque el tipo es fuerte y aunque haya sido inferido no se puede modificar
+
+// Elementos de array
+
+let frutas: string[]; // El tipo será array de elementos string
+
+frutas = ['Pera', 'Manzana', 'Naranja'];
+
+let resultados: Array<number> // Otra forma de tipar los elementos de un array
+
+resultados = [9.4, 10, 8, 6];
+
+// Any (para eliminar la inferencia)
+
+// let edad = '40'; 
+// edad = 37;
+
+let edad: any = '40'; // El tipo any permite cualquier tipo (volver a tipado dinámico)
+edad = 37;
+
+// Void para tipar que una función no devuelva nada
+
+function saludo (): void {
+    console.log('Hola Mundo!'); 
+    // return 'Hola Mundo!';
+}
+
+// Tipado de objetos
+
+let jugador1: object; // objeto de cualquier tipo
+
+jugador1 = {
+    nombre: 'Lionel',
+    apellidos: 'Messi'
+}
+
+let jugador2: {nombre: string, apellidos: string}; // objeto de una determinada forma
+
+jugador2 = {
+    nombre: 'Cristiano',
+    apellidos: 'Ronaldo'
+    // edad: 40 no podemos poner propiedades que no existen en el tipo
+}
